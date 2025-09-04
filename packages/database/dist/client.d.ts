17bc05493504ce4e8ca821108c6c3d5b6842b806
lolo298 <lorenzoaoulini@gmail.com>
2025-09-04 09:40:09 +0200

    Remove cached

--- packages/database/dist/client.d.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "../generated/client";
-declare const extendedPrisma: PrismaClient<import("../generated/client").Prisma.PrismaClientOptions, never, import("../generated/client/runtime/library").DefaultArgs>;
-type ExtendedPrismaClient = typeof extendedPrisma;
-export declare const prisma: ExtendedPrismaClient;
-export {};
